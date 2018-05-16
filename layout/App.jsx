b35b8e75a12fddecd62669032e77e6cd08844b18
Rob Brennan <rob@therobbrennan.com>
2018-05-15 19:01:35 -0700

    Removed unnecessary links to ping and people pages in the layout component

--- layout/App.jsx
+++ layout/App.jsx
@@ -4,11 +4,6 @@ export default ({ children }) => {
   return (
     <div>
       <Link href={"/"}><a>Home</a></Link>&nbsp;
-      <Link href={"/ping"}><a>Ping</a></Link>&nbsp;
-      <Link as={`/people`} href={`/ping`}><a>People</a></Link>&nbsp;
-      <Link as={`/people/developers`} href={`/ping?slug=developers`}><a>Developers</a></Link>&nbsp;
-      <Link as={`/people/developers/rob`} href={`/ping?slug=developers&name=rob`}><a>Rob</a></Link>&nbsp;
-
       {children}
       <style>{`
       /* Sample inline styles */
