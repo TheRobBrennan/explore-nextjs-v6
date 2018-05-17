227d9a22d6f773d6d974bf6c9f0b25cc56f0b135
Rob Brennan <rob@therobbrennan.com>
2018-05-16 17:51:30 -0700

    Converted App layout component to TypeScript

--- layout/App.tsx
+++ layout/App.tsx
@@ -1,10 +1,10 @@
-import Link from "next/link"
+import Link from 'next/link';
 
 export default ({ children }) => {
   return (
     <div>
-      <Link href={"/"}><a>Home</a></Link>&nbsp;
-      <Link href={"/ping"}><a>Ping</a></Link>&nbsp;
+      <Link href={'/'}><a>Home</a></Link>&nbsp;
+      <Link href={'/ping'}><a>Ping</a></Link>&nbsp;
       <Link as={`/people`} href={`/ping`}><a>People</a></Link>&nbsp;
       <Link as={`/people/developers`} href={`/ping?slug=developers`}><a>Developers</a></Link>&nbsp;
       <Link as={`/people/developers/rob`} href={`/ping?slug=developers&name=rob`}><a>Rob</a></Link>&nbsp;
@@ -21,5 +21,5 @@ export default ({ children }) => {
     `}
     </style>
     </div>
-  )
-}
+  );
+};
